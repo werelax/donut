62c0e5df2db2db49a3592cd8a2df83b36a1d7640
Elías Alonso <eliasagc@gmail.com>
2012-08-04 04:44:03 +0200

    forgot to add the new code-walker experiment

--- repl.js
+++ repl.js
@@ -24,10 +24,9 @@ read_file("./lib/prelude.js", 'utf-8', function(err, data) {
 
     read_file('./lib/prelude.dt', 'utf-8', function(err, data) {
       if (err) throw err;
-      var prelude = read(data).map(compile).concat('').join(";\n");
+      var prelude = data + "\n";
 
       vm.runInContext(js_prelude, repl_context);
-      vm.runInContext(prelude, repl_context);
 
       console.log("\n\n (·) DONUT 0.0.1 :: Welcome!\n\n");
 
@@ -35,15 +34,9 @@ read_file("./lib/prelude.js", 'utf-8', function(err, data) {
         rl.question('DONUT * ', function(ans) {
           var result, ast, final_ast, compiled;
           if (ans.trim()) try {
-            ast = read(ans);
-            final_ast = macroexpand(ast);
+
+            final_ast = macroexpand(read(prelude + ans));
             compiled = final_ast.map(compile).concat('').join(";\n");
-
-            console.log(format("  * AST: %j", ast));
-            console.log(format("  * EXPANDED: %j", final_ast));
-            console.log(format("  * JS: %s", decorate(compiled)));
-
-            console.log("");
 
             result = vm.runInContext(compiled, repl_context);
 
